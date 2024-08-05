9217bee9b970d3333a0c649b98d82f945172ced6
Farhan Helmy <farhanhlmy@gmail.com>
2024-08-05 17:33:37 +0800

    :adhesive_bandage: change url

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -106,7 +106,7 @@ function Header() {
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className="hidden lg:block">
-          <Link href={"google.com"} target="_blank">
+          <Link href="/dashboard" target="_blank">
             <div className="font-bold">NGOMS Dashboard</div>
           </Link>
         </div>
@@ -115,7 +115,7 @@ function Header() {
         </div>
 
         <div className="flex items-center gap-2">
-            <ToggleTheme />
+          <ToggleTheme />
           <UserButton
             afterSignOutUrl="/"
             appearance={{
