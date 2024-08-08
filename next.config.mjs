d21c182826ad9a29af34cab69fca8e2b3233be58
khairin <khairin13chan@gmail.com>
2024-08-08 09:58:21 +0800

    :zap: organization card

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'mrds.org.my'
-            },
-            {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com'
-            }
-        ]
-    }
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mrds.org.my",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
