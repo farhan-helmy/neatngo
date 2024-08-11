6bdbdbed91b866e60f2751b0e1e24284ffb57fd8
Farhan Helmy <farhanhlmy@gmail.com>
2024-08-11 15:30:15 +0800

    :sparkles: Add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,37 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/layout/theme-provider";
-import "./globals.css";
-import { ClerkProvider, SignedOut } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NGO Management System",
-  description: "A SaaS for managing your NGO",
+  title: "NeatNGO - Efficient NGO Management System",
+  description:
+    "Ditch excel sheets and streamline your NGO operations with our comprehensive management solution.",
+  openGraph: {
+    title: "NeatNGO - Efficient NGO Management System",
+    description:
+      "Ditch excel sheets and streamline your NGO operations with our comprehensive management solution.",
+    type: "website",
+    url: "https://neatngo.com",
+    images: [
+      {
+        url: "https://neatngo.com/api/og",
+        width: 1200,
+        height: 630,
+        alt: "NeatNGO Open Graph Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NeatNGO - Efficient NGO Management System",
+    description:
+      "Ditch excel sheets and streamline your NGO operations with our comprehensive management solution.",
+    images: ["https://neatngo.com/api/og"],
+  },
 };
 
 export default function RootLayout({
