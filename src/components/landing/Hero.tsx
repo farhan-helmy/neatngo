eee03b750763be786e2b7b30271986df88263bf2
Farhan Helmy <farhanhlmy@gmail.com>
2024-08-17 13:07:32 +0800

    :hammer: test pipeline watchtower

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -20,7 +20,7 @@ export const Hero = () => {
           </h1>
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              - NGO Management System
+              AUTO DEPLOY TO DEMO IS WORKING
             </span>{" "}
           </h2>
         </main>
