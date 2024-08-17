341f4b0283e4af44998dc9e0b12cfd87c4a768f9
Farhan Helmy <farhanhlmy@gmail.com>
2024-08-17 13:40:01 +0800

    :sparkles: Change title for demo instance

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -20,7 +20,8 @@ export const Hero = () => {
           </h1>
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              Ariff Naznhan
+              - NGO Management System{" "}
+              {process.env.NEXT_PUBLIC_ENV === "dev" && "DEMO VERSION"}
             </span>{" "}
           </h2>
         </main>
