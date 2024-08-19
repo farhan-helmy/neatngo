c6a10f4057ad4c437b9bb57e76ca12eec0d1f2f9
Farhan Helmy <59960562+farhan-helmy@users.noreply.github.com>
2024-08-19 09:51:17 +0800

    :sparkles: Activate public ngo listings at homepage (#29)

---
+++ drizzle/0006_spicy_iron_fist.sql
@@ -0,0 +1,15 @@
+ALTER TABLE "event_registrations" DROP CONSTRAINT "event_registrations_event_id_events_id_fk";
+--> statement-breakpoint
+ALTER TABLE "events" DROP CONSTRAINT "events_organization_id_organizations_id_fk";
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "event_registrations" ADD CONSTRAINT "event_registrations_event_id_events_id_fk" FOREIGN KEY ("event_id") REFERENCES "public"."events"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "events" ADD CONSTRAINT "events_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
