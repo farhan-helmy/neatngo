70bc7c45abf36bcb9e6a1ab88caf0acacc571d2f
Farhan Helmy <farhanhlmy@gmail.com>
2024-08-20 12:17:13 +0800

    :ambulance: fix logo function

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -158,7 +158,7 @@ function Header() {
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className="hidden lg:block">
-          <Link href="/dashboard" target="_blank">
+          <Link href="/dashboard">
             <div className="font-bold">
               <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
                 NeatNGO
